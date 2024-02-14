a4ed996372dba9ec953916081cc12bc601fb074b
Purvesh Prajapati <ppurvesh7@gmail.com>
2024-02-15 00:32:22 +0530

    changes done in classes and changed mobile number and mail id in the website

--- views/wp-content/themes/catera/assets/js/elementor/brandf269.js
+++ views/wp-content/themes/catera/assets/js/elementor/brandf269.js
@@ -1,1 +1,1 @@
-(function($){"use strict";$(window).on('elementor/frontend/init',()=>{const addHandler=($element)=>{elementorFrontend.elementsHandler.addHandler(cateraSwiperBase,{$element,});};elementorFrontend.hooks.addAction('frontend/element_ready/catera-brand.default',addHandler);});})(jQuery);
+(function($){"use strict";$(window).on('elementor/frontend/init',()=>{const addHandler=($element)=>{elementorFrontend.elementsHandler.addHandler(royaltreattSwiperBase,{$element,});};elementorFrontend.hooks.addAction('frontend/element_ready/royaltreatt-brand.default',addHandler);});})(jQuery);
